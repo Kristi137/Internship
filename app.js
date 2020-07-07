6b4b40ea45ea6b8fe974ac05c5ab78667cf913ef
Popescu Cristi <shippo658@gmail.com>
2020-07-07 17:39:11 +0300

    hide and show functionality for lists

---
+++ app.js
@@ -0,0 +1,9 @@
+function showul (event) {
+    event.stopPropagation();
+    let nextul = event.target.nextElementSibling;
+    if(nextul.style.display == "block") {
+      nextul.style.display = "none"; 
+    } else {
+      nextul.style.display = "block";
+    }
+  }
