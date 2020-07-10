d910c507ea761c0c504dec9196d552484731077f
Popescu Cristi <shippo658@gmail.com>
2020-07-10 13:03:24 +0300

    Modal Window for photos

--- app.js
+++ app.js
@@ -8,3 +8,34 @@ function showul (event) {
     }
   }
 
+  let modal = document.querySelector(".modal");
+  let trigger = document.querySelectorAll(".trigger");
+  let closeButton = document.querySelector(".close-button");
+  
+  trigger.forEach(function(el){
+    el.addEventListener("click", function() {
+      modal.classList.toggle("show-modal");
+      let modalContent = document.querySelector('.modal-content');
+      let oldImg = document.querySelector('.oldImg');
+      let url = el.src;
+      url = url.replace('/150/', '/600/');
+      let newImg = document.createElement('img');
+      newImg.src = url;
+      newImg.id = 'newImg';
+      modalContent.appendChild(newImg);		
+    });
+  });
+  closeButton.addEventListener("click", function() {
+    modal.classList.toggle("show-modal");
+    let modalContent = document.querySelector('.modal-content');
+    let myImg = document.querySelector('#newImg');
+    modalContent.removeChild(myImg);
+  });
+  window.addEventListener("click", function(event) {
+        if (event.target === modal) {
+        modal.classList.toggle("show-modal");
+        let modalContent = document.querySelector('.modal-content');
+        let myImg = document.querySelector('#newImg');
+        modalContent.removeChild(myImg);
+    }
+  });
